0bb373967cb229dffd694f29c8bed13ce8d7e7cb
Victor Caio <kaioaraujo426@gmail.com>
2022-09-21 09:32:35 -0300

    feat: add create endpoint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Tudo ok" });
+const customers = [];
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'Tudo ok' });
+});
+
+app.post('/account', (req, res) => {
+  const { cpf, name } = req.body;
+  const _id = uuidv4();
+
+  customers.push({ cpf, name, _id, statement: [] });
+
+  return res.status(201).send({ message: 'created' });
 });
 
 app.listen(3333);
